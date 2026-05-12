195d565837f0630fedf5706182e89d7bb232d3ff
Asim Ali <asimalizamurani@gmail.com>
2026-05-12 22:10:22 +0500

    core components of react native done

--- app/index.tsx
+++ app/index.tsx
@@ -1,59 +1,61 @@
-import { useState } from "react";
-import { View, Text, Image, TextInput, Pressable } from "react-native";
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, Pressable } from 'react-native'
+import React from 'react'
 
-export default function HomeScreen() {
-
-    const [name, setName] = useState("");
-
+const HomeScreen = () => {
   return (
-    <View>
-      <Text numberOfLines={3}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta laborum quos quia corporis, ducimus unde delectus nobis molestiae exercitationem perferendis accusamus labore ad optio sint voluptas. Hic enim distinctio sunt?
-      Quam quasi ratione, suscipit delectus adipisci similique optio excepturi autem rem maxime sed non dolores explicabo alias possimus ipsam qui asperiores </Text>
-
-      <Image source={{uri: 'https://images.unsplash.com/photo-1778392120781-a6b2b55dc12a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8'}}
-      width={300}
-      height={200}
-      />
-
-      <Image
-      source={require("@/assets/images/icon.png")}
+    <KeyboardAvoidingView style={{
+      flex: 1
+    }}>
+      <View
       style={{
-        width: 100,
-        height: 100,
+        flex: 1, justifyContent: 'flex-end', padding: 24
       }}
-      blurRadius={20}
-      />
+      >
+        <Text
+        style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20}}
+        >Login</Text>
 
-      <TextInput
-      placeholder="Enter your name"
-      value={name}
-      onChangeText={setName}
-      placeholderTextColor={"blue"}
-      
-      style={{
-        borderWidth: 1,
-        borderColor: "gray",
-        padding: 10,
-        marginTop: 20,
-        fontSize: 20,
-      }}
-      />     
+        <TextInput
+        placeholder='name'
+        style={{
+          borderWidth: 1,
+          borderColor: "grey",
+          borderRadius: 10,
+          padding: 12,
+          fontSize: 16,
+          marginBottom: 12
+        }}
+        />
 
-      <Pressable onPress={() => alert("button pressed")}
-        style={
-          (pressed) => ({
-            backgroundColor: pressed ? "red" : "grey",
-          })
-        }
-        hitSlop={{
-          top: 20,
-          bottom: 60,
+        <TextInput
+        placeholder='email'
+        style={{
+          borderWidth: 1,
+          borderColor: "grey",
+          borderRadius: 10,
+          padding: 12,
+          fontSize: 16,
+          marginBottom: 12
+        }}
+        />
+
+        <Pressable
+        style={{
+          borderRadius: 10,
+          padding: 14,
+          alignItems: 'center',
+          backgroundColor: "purple"
         }}
         >
-        {({pressed}) => 
-          pressed ? <Text> Press me</Text> : <Text> Pressed me </Text>}
-      </Pressable>
-    
-    </View>
+          <Text
+          style={{ color: "white", fontWeight: 'bold', fontSize: 16}}
+          >Sign in</Text>
+        </Pressable>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
